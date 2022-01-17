4e75e46d2f90117dc590776f1829e377aa3c92a5
JoyceQuintino <joycequintino11@gmail.com>
2022-01-16 21:41:31 -0300

    Criação do componente Footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../theme';
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 70,
+        backgroundColor: COLORS.GREEN,
+        position: 'absolute',
+        bottom: 0,
+        alignItems: 'center'
+    }
+});
+
+export default styles;
