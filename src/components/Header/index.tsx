198e6c1087a03b11aac170474aaf72d2bbb61de8
JoyceQuintino <joycequintino11@gmail.com>
2022-01-16 21:41:11 -0300

    Criação do componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+function Header(){
+    return(
+        <View style={styles.header}>
+
+        </View>
+    )
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../theme';
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 90,
+        backgroundColor: COLORS.GREEN,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default styles;
